f9521b1da174f72fae10acdb12046f510bb73c4b
jcoll059 <jordancolledge059@gmail.com>
2022-09-16 11:17:40 -0700

    Clarifying longest sequence vs. longest number

--- src/collatz/Main.java
+++ src/collatz/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Collatz Conjecture (or 3n+1 Problem) simulator!");
-		System.out.println("Would you like to simulate a single number, or find the longest number up to a particular number?");
+		System.out.println("Would you like to simulate a single number, or find the longest sequence up to a particular number?");
 		System.out.println("Type 'a' to simulate one number, or 'b' to find the longest.");
 		
 		Scanner sc = new Scanner(System.in);
