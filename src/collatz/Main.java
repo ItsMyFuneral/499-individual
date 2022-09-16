55c9910cae536a84edf06d714d74aafc284c1388
jcoll059 <jordancolledge059@gmail.com>
2022-09-16 11:08:52 -0700

    Added "3n+1 Problem" reference to initial string printout

--- src/collatz/Main.java
+++ src/collatz/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Collatz Conjecture simulator!");
+		System.out.println("Welcome to the Collatz Conjecture (or 3n+1 Problem) simulator!");
 		System.out.println("Would you like to simulate a single number, or find the longest number up to a particular number?");
 		System.out.println("Type 'a' to simulate one number, or 'b' to find the longest.");
 		
